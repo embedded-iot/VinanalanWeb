003868dfa9d1e58f8efa32b3456e82f556126a5d
embedded-iot <nguyenquan5895@gmail.com>
2019-03-02 15:49:10 +0700

    [020319] Add Table Custom Component.

---
+++ src/components/commons/TableCustom/TableCustom.js
@@ -0,0 +1,63 @@
+import {Button, Input, Table, Select } from 'antd';
+import React, {Component} from "react";
+import './TableCustom.scss'
+const Option = Select.Option;
+const Search = Input.Search;
+
+const defaultProps = {
+  columns: [],
+  dataSource: [],
+  bordered: true,
+  rowKey: record => record.login.uuid,
+  pagination: {},
+  loading: false,
+  onChange: () => { }
+}
+
+const pageSizes = [
+  { title: "10 hàng", value: 10},
+  { title: "15 hàng", value: 15},
+  { title: "20 hàng", value: 20},
+];
+
+export default class TableCustom extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  setPageSize = pageSize => {
+    let { pagination, filters, sorter, searchText } = {...this.props.tableSettings};
+    let newPagination = { ...pagination, pageSize};
+    this.props.onChange(newPagination, filters, sorter, {}, searchText);
+  }
+
+  onSearchText = text => {
+    console.log("searchText", text)
+    let { pagination, filters, sorter} = {...this.props.tableSettings};
+    this.props.onChange(pagination, filters, sorter, {}, text ? text : null);
+  }
+
+  render() {
+    const config = { ...defaultProps, ...this.props };
+    return (
+      <div className="table-custom-wrapper">
+        <div className="table-header">
+          <Search
+            placeholder="Nhập thông tin tìm kiếm"
+            onSearch={this.onSearchText}
+            style={{ width: 300 }}
+          />
+          <div className="dropdown-page-size">
+            <span>Hiển thị</span>
+            <Select defaultValue={pageSizes[0].value} style={{ width: 120 }} onChange={this.setPageSize}>
+              {pageSizes.map((item, index) => <Option key={index} value={item.value}>{item.title}</Option>)}
+            </Select>
+          </div>
+        </div>
+        <Table {...config}/>
+      </div>
+    );
+  }
+}
+
