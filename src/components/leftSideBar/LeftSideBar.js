c0937a4c34a5e53733fbdae4ccb0a13023355c74
embedded-iot <nguyenquan5895@gmail.com>
2019-03-05 01:33:06 +0700

    [050319] Update text.

--- src/components/leftSideBar/LeftSideBar.js
+++ src/components/leftSideBar/LeftSideBar.js
@@ -83,8 +83,8 @@ class LeftSideBar extends Component {
                         </div>
                         <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
                             <Link className="dropdown-item" to="/Users" onClick={() => this.handleActive("/Users")}>Quản lý tài khoản</Link>
-                            <Link className="dropdown-item" to="/RoomsCatalog" onClick={() => this.handleActive("/RoomsCatalog")}>{STRINGS.ROOM_CATALOG}</Link>
-                            <Link className="dropdown-item" to="/HomeCatalog" onClick={() => this.handleActive("/HomeCatalog")}>{STRINGS.HOME_CATALOG}</Link>
+                            <Link className="dropdown-item" to="/RoomsCatalog" onClick={() => this.handleActive("/RoomsCatalog")}>Loại phòng</Link>
+                            <Link className="dropdown-item" to="/HomeCatalog" onClick={() => this.handleActive("/HomeCatalog")}>Loại tòa nhà</Link>
                             <Link className="dropdown-item" to="/IncomeUtilities" onClick={() => this.handleActive("/IncomeUtilities")}>Tiện ích trong</Link>
                             <Link className="dropdown-item" to="/OutcomeUtilities" onClick={() => this.handleActive("/OutcomeUtilities")}>Tiện ích ngoài</Link>
                             <Link className="dropdown-item" to="/ExtraFees" onClick={() => this.handleActive("/ExtraFees")}>Các loại phí khác</Link>
