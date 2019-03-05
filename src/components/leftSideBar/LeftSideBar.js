fc56e31d508b40f622dc2885920f928dc4838c3e
embedded-iot <nguyenquan5895@gmail.com>
2019-03-05 23:50:44 +0700

    [050319] Apply style yellow.

--- src/components/leftSideBar/LeftSideBar.js
+++ src/components/leftSideBar/LeftSideBar.js
@@ -8,6 +8,9 @@ import icSetting from "../../public/images/icons/menu-setting-icon-158259.png";
 import icService from "../../public/images/icons/ic_service.png";
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
+import { Menu, Icon, Button } from 'antd';
+
+const SubMenu = Menu.SubMenu;
 
 const STRINGS = {
     HOME: <FormattedMessage id="HOME" />,
@@ -24,8 +27,15 @@ class LeftSideBar extends Component {
         super(props);
 
         this.state = {
+            collapsed: false,
             urlPage: location.pathname
         }
+    }
+
+    toggleCollapsed = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
     }
 
     handleLogOut = () => {
@@ -37,8 +47,60 @@ class LeftSideBar extends Component {
         this.setState({ urlPage: value });
     }
 
-
+    getClassNameLeftSideBar = () => {
+        let classNameLeftSideBar = 'left-side-bar';
+        if (location.pathname === "/Login" || location.pathname === "/Register") {
+            classNameLeftSideBar += ' hide';
+        }
+        if (this.state.collapsed) {
+            classNameLeftSideBar += ' collapsed';
+        }
+        return classNameLeftSideBar;
+    }
     render() {
+        const { urlPage, collapsed } = this.state;
+        const widthLeftMenu = collapsed ? "80px" : "290px";
+        return (
+          <div className={this.getClassNameLeftSideBar()} style={{width: widthLeftMenu}}>
+              <div className="menu-content" style={{width: widthLeftMenu}}>
+                  <Menu
+                    defaultSelectedKeys={['1']}
+                    defaultOpenKeys={['sub1']}
+                    mode="inline"
+                    theme="light"
+                    inlineCollapsed={this.state.collapsed}
+                  >
+                      {/*<Menu.Item key="1">
+                          <Icon type="pie-chart" />
+                          <span>Option 1</span>
+                      </Menu.Item>
+                      <Menu.Item key="2">
+                          <Icon type="desktop" />
+                          <span>Option 2</span>
+                      </Menu.Item>
+                      <Menu.Item key="3">
+                          <Icon type="inbox" />
+                          <span>Option 3</span>
+                      </Menu.Item>*/}
+                      <SubMenu key="sub1" title={<span><img className="anticon anticon-inbox" style={{ width: '19px', height: "19px" }} src="https://cdn0.iconfinder.com/data/icons/essen/32/settings.png" /><span>Cấu hình</span></span>}>
+                          <Menu.Item key="7"><Link className="dropdown-item" to="/Users" onClick={() => this.handleActive("/Users")}>Quản lý tài khoản</Link></Menu.Item>
+                          <Menu.Item key="8"><Link className="dropdown-item" to="/RoomsCatalog" onClick={() => this.handleActive("/RoomsCatalog")}>Loại phòng</Link></Menu.Item>
+                          <Menu.Item key="9"><Link className="dropdown-item" to="/HomeCatalog" onClick={() => this.handleActive("/HomeCatalog")}>Loại tòa nhà</Link></Menu.Item>
+                          <Menu.Item key="10"><Link className="dropdown-item" to="/IncomeUtilities" onClick={() => this.handleActive("/IncomeUtilities")}>Tiện ích trong</Link></Menu.Item>
+                          <Menu.Item key="11"><Link className="dropdown-item" to="/OutcomeUtilities" onClick={() => this.handleActive("/OutcomeUtilities")}>Tiện ích ngoài</Link></Menu.Item>
+                          <Menu.Item key="12"><Link className="dropdown-item" to="/ExtraFees" onClick={() => this.handleActive("/ExtraFees")}>Các loại phí khác</Link></Menu.Item>
+                      </SubMenu>
+                  </Menu>
+                  <Button type="primary" className="button-collapsed" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
+                      <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
+                  </Button>
+              </div>
+
+          </div>
+        )
+    }
+
+    /*render() {
         let hideMenu = false;
         if (location.pathname === "/Login" || location.pathname === "/Register") {
             hideMenu = true;
@@ -62,12 +124,12 @@ class LeftSideBar extends Component {
                         <span className="menu">{STRINGS.ROOM}</span>
                         <hr />
                     </Link>
-                    {/*<div onClick={(e) => this.handleRedirectPage("/Service")}*/}
-                    {/*className={urlPage === "/service" ? "item-menu selected" : "item-menu"}>*/}
-                    {/*<img src={icService}/>*/}
-                    {/*<span className="menu">Service</span>*/}
-                    {/*<hr/>*/}
-                    {/*</div>*/}
+                    {/!*<div onClick={(e) => this.handleRedirectPage("/Service")}*!/}
+                    {/!*className={urlPage === "/service" ? "item-menu selected" : "item-menu"}>*!/}
+                    {/!*<img src={icService}/>*!/}
+                    {/!*<span className="menu">Service</span>*!/}
+                    {/!*<hr/>*!/}
+                    {/!*</div>*!/}
                     <Link to="/Report" onClick={() => this.handleActive("/Report")}
                         className={urlPage === "/Report" ? "item-menu selected" : "item-menu"}>
                         <img src={icReport} />
@@ -93,7 +155,7 @@ class LeftSideBar extends Component {
                 </div>
             </div>
         );
-    }
+    }*/
 }
 
 export default LeftSideBar;
