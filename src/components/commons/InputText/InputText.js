4cad1b16321f844d1ad5f6dde946d7cdc2a0c56f
embedded-iot <nguyenquan5895@gmail.com>
2019-03-19 00:28:28 +0700

    [170319] Update Input Text component.

--- src/components/commons/InputText/InputText.js
+++ src/components/commons/InputText/InputText.js
@@ -17,13 +17,15 @@ class InputText extends Component {
   render() {
     const { title, isRequired, value, placeholder, titleInfo, placeholderInfo, isSubmitted, disabled} = this.props;
     return (
-      <div className="input-text-wrapper">
-        <div className="heading">{ title }
-          { isRequired && <span className="is-required">*</span> }
-          {
-            titleInfo && (<Tooltip placement={ placeholderInfo || "top" } title={titleInfo}><Icon type="info-circle" /></Tooltip>)
-          }
-        </div>
+      <div className="input-text-wrapper" style={ !title ? {margin: 0} : {}}>
+        {
+          !!title && (<div className="heading">{ title }
+            { isRequired && <span className="is-required">*</span> }
+            {
+              titleInfo && (<Tooltip placement={ placeholderInfo || "top" } title={titleInfo}><Icon type="info-circle" /></Tooltip>)
+            }
+          </div>)
+        }
         <Input value={value} onChange={ this.handleOnChange } placeholder={placeholder} disabled={disabled}/>
         {isSubmitted && isRequired && !value && <span style={{ color: "red" }}>{STRINGS.REQUIRED_ALERT}</span>}
       </div>
